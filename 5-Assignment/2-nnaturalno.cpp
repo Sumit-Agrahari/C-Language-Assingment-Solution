b99623316b1641d86f87e2d6a03175270b53239b
Sumit Kumar Agrahari <83569086+Sumit-Agrahari@users.noreply.github.com>
2022-07-18 00:33:33 +0530

    Add files via upload

---
+++ 5-Assignment/2-nnaturalno.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    int i=1;
+    while(i<=n){
+      printf("%d\n",i);
+      i++;
+    }
+    return 0;
+}
+    
