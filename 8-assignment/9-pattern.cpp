0e63521b086de246da57daaaf19e8f88370d0c25
Sumit Kumar Agrahari <83569086+Sumit-Agrahari@users.noreply.github.com>
2022-07-28 18:38:20 +0530

    Add files via upload

---
+++ 8-assignment/9-pattern.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,count;
+    for(i=0;i<=3;i++)
+    {
+        count=0;
+        for(j=0;j<i;j++){
+            printf(" ");
+        }
+        for(j=0;j<=3-i;j++){
+             printf("%d",++count);
+        }
+        count--;
+        while(count){
+            printf("%d",count--);
+        }
+        printf("\n");
+    }
+    
+
+    return 0;
+
+} 
